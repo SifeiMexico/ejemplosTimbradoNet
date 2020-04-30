10237c1fb2728765586792cef3abd2d51a66a857
Daniel Hernandez <daniel.hernandez.job@gmail.com>
2020-04-30 13:06:35 -0500

    ejemplo de timbrado y cancelacion listos

---
+++ Sifei.Timbrado.Ws.ClienteEjemplo/InvokeWsForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Sifei.Timbrado.Ws.ClienteEjemplo {
+    partial class InvokeWsForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTimbrar = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTimbrar
+            // 
+            this.btnTimbrar.Location = new System.Drawing.Point(80, 118);
+            this.btnTimbrar.Name = "btnTimbrar";
+            this.btnTimbrar.Size = new System.Drawing.Size(75, 23);
+            this.btnTimbrar.TabIndex = 0;
+            this.btnTimbrar.Text = "Timbrar";
+            this.btnTimbrar.UseVisualStyleBackColor = true;
+            this.btnTimbrar.Click += new System.EventHandler(this.btnTimbrar_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(229, 223);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // InvokeWsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnTimbrar);
+            this.Name = "InvokeWsForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTimbrar;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Sifei.Timbrado.Ws.ClienteEjemplo/InvokeWsForm.cs
@@ -0,0 +1,147 @@
+﻿using IniParser;
+using IniParser.Model;
+using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Web.Services.Protocols;
+using System.Windows.Forms;
+
+/// <summary>
+/// 
+/// Clase ejemplo de consumo de timbrado
+/// </summary>
+namespace Sifei.Timbrado.Ws.ClienteEjemplo {
+    public partial class InvokeWsForm : Form {
+
+        public IniData Config { get; set; }
+        public InvokeWsForm()
+        {
+            InitializeComponent();
+            loadConnectionData();
+        }
+
+        public string TimbradoPath { get; set; }
+        /// <summary>
+        /// Este metodo obtiene los datos de conexion de un archivo .ini, en produccion NO hacer esto.
+        /// </summary>        
+        public void loadConnectionData()
+        {
+
+            var parser = new FileIniDataParser();
+            Config = parser.ReadFile("./config.ini");
+            PathCancelacion = "./cancelacion";
+            TimbradoPath = "./timbrado";
+
+            Directory.CreateDirectory(TimbradoPath);//me aseguro que exista
+
+            //estructura de archivo ini.
+            /**
+            
+             * 
+             */
+        }
+
+
+        private void btnTimbrar_Click(object sender, EventArgs e)
+        {
+            var service = new Cliente.timbrado.SIFEIService();
+            var usuario = Config["timbrado"]["UsuarioSIFEI"];
+            var password = Config["timbrado"]["PasswordSIFEI"];
+            var serie = "";
+            var idEquipoGenerado = Config["timbrado"]["IdEquipoGenerado"];
+
+            //leemos el archivo
+
+            byte[] archivoXml= File.ReadAllBytes("./assets/cfdi.xml");
+            //Ya que la respuesta devuelve un zip debemos de extraer en contenido
+            try { 
+            byte[] respuestaZip= service.getCFDI(
+                usuario,
+                password,
+                archivoXml,
+                serie,
+                idEquipoGenerado
+            );
+
+                Stream zs = new MemoryStream(respuestaZip);
+                Stream xmlS = new MemoryStream();
+
+                //se extrae el xml, que es el unico archivo zip.
+                using (ZipFile zip = ZipFile.Read(zs))
+                {
+                    foreach (ZipEntry ze in zip) {
+                        ze.Extract(TimbradoPath);                        
+                        break;                        
+                    }
+                }
+                MessageBox.Show("CFDI timbrado");               
+
+            }catch(SoapException ex)
+            {
+                MessageBox.Show(
+                                    ex.Message +
+                                    ex.Detail.FirstChild.SelectSingleNode("codigo").InnerText +
+
+                                    ex.Detail.FirstChild.SelectSingleNode("error").InnerText
+
+
+                                    , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error
+                                    );
+            }
+
+
+        }
+        public string PathCancelacion { get; set; }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            var usuario = Config["timbrado"]["UsuarioSIFEI"];
+            var password = Config["timbrado"]["PasswordSIFEI"];
+            var idEquipoGenerado = Config["timbrado"]["IdEquipoGenerado"];
+
+            //Para este ejemplo el pfx se lee desde el sistema de archivos, el origen puede ser cualquiera que sea seguro.
+            var passwordPfx = Config["cancelacion"]["PFXPass"];
+            var pfxBytes = File.ReadAllBytes(Config["cancelacion"]["PFX"]); // a manera de ejemlo se lee desde el siste de archivos
+            var uuids = new List<string>();
+            uuids.Add("5d100c8d-1b19-4e5c-9748-6116fdbe5465");
+
+            var c = new Cancelacion.Ws.Client.Cancelacion.Cancelacion();
+            //el metodo devuelve un acuse de cancelacion 
+
+            try
+            {
+                var acuseXML = c.cancelaCFDI(
+                      usuario, //usuario sifei
+                      password, //contraseña
+                      "RFC",
+                      pfxBytes, //bytes del archivo pfx
+                      passwordPfx, //pasword del pfx, por ende el pfx debe ser creado con contraseña
+                      uuids.ToArray() //array del UUID de XML  a cancelar.
+                 );
+                //en este caso se ejemplifica como guardar el XML en una ruta
+                Directory.CreateDirectory(PathCancelacion);
+                String acuseFileName = Path.Combine(PathCancelacion, "acuseCancelacion" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".xml");
+                File.WriteAllText(acuseFileName, acuseXML, Encoding.UTF8);
+                MessageBox.Show(string.Format("Cancelacion Exitosa, acuse guardado en {0}", acuseFileName));
+            }
+            catch (SoapException ex)
+            {
+                //obtenemos la excepcion
+                MessageBox.Show(
+                    ex.Message+
+                    ex.Detail.FirstChild.SelectSingleNode("codigo").InnerText +
+
+                    ex.Detail.FirstChild.SelectSingleNode("error").InnerText 
+
+
+                    , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error
+                    );
+
+            }
+        }   
+    }
+}
